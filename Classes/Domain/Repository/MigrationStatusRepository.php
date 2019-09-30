abcbe6b282aecea9c965f1f11b8ec969ed5bc6db
Tobias Wojtylak <tobias.wojtylak@netlogix.de>
2019-09-30 15:26:25 +0200

    FEATURE: Add MigrationStatusRepostitory
    Related: ADIGITAL-790

---
+++ Classes/Domain/Repository/MigrationStatusRepository.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Netlogix\Migrations\Domain\Repository;
+
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Persistence\Repository;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class MigrationStatusRepository extends Repository
+{
+
+}
